3ae6a9d80add13276bbda2109d0c94cb04e4d8b5
Timothy J Fontaine <tjfontaine@gmail.com>
2013-05-04 23:53:34 +0000

    add some logging to ghapi

--- ghapi.js
+++ ghapi.js
@@ -2,11 +2,15 @@ var GHApi = module.exports = function (options) {
   if (!(this instanceof GHApi)) return new GHApi(options);
   this.options = options;
 
+  this.log = options.log.child({plugin: 'ghapi'});
+
   this.options.server.get('/ghapi/.*', this.proxy.bind(this))
 };
 
 GHApi.prototype.proxy = function (req, res, next) {
   var url = '/repos' + req.path().replace(/^\/ghapi/, '');
+
+  this.log.info({url: url});
 
   this.options.ghrest.get(url, function (e, jreq, jres, b) {
     res.json(jres.statusCode, b);
